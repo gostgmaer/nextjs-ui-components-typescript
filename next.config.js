211b7f6d1421af65fb32fedd15578de925f04397
kishor.sarkar <kishor.sarkar@inadev.com>
2025-05-31 21:19:36 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
   images: { unoptimized: true },
 };
 
